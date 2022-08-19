c7f2644af0914e8541a6a578bf5118eaf717aad3
Black Light <blacklight@Mac-mini.local>
2022-08-19 16:02:26 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const functions = require("firebase-functions");
 
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+const port = 3000;
+const server = app.listen(port, () => {
+        console.log('listening on port %s.\n', server.address().port);
+    });
+
+module.exports = app;
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
