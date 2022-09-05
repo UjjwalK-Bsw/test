39e48efd4f13797e913c31a58f90ee9c15d9dab5
UjjwalK-Bsw <ujjwal@bswgames.com>
2022-09-05 18:00:14 +0530

    update

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-const functions = require("firebase-functions");
 
 const express = require('express');
 
@@ -14,8 +13,3 @@ const server = app.listen(port, () => {
     });
 
 module.exports = app;
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
